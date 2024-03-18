d743940de2e8aa9b839b957c62c64e71824f96a9
Nils Kriedner <nkriedner@googlemail.com>
2024-03-18 18:18:07 +0100

    Create header in App + import provided styles + install react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -23,6 +24,26 @@ function App() {
 
     return (
         <div className="App">
+            <header>
+                <div className="wrap header--flex">
+                    <h1 className="header--logo">
+                        {/* <Link to="/">Courses</Link> */}
+                        <span>Courses</span>
+                    </h1>
+                    <nav>
+                        <ul className="header--signedout">
+                            <li>
+                                {/* <Link to="sign-up.html">Sign Up</Link> */}
+                                <span>Sign Up</span>
+                            </li>
+                            <li>
+                                {/* <Link to="sign-in.html">Sign In</Link> */}
+                                <span>Sign In</span>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
             {courses && console.log("courses:", courses)}
 
             {/* when there are courses -> present the title of the courses */}
