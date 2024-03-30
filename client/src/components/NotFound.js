464cb6a02a52bc48ae740bfe3230cf3fe41b930d
Nils Kriedner <nkriedner@googlemail.com>
2024-03-30 13:42:06 +0100

    Add html from mockup to NotFound

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,5 +1,12 @@
 const NotFound = () => {
-    return <h2>The requested page cannot be found.</h2>;
+    return (
+        <main>
+            <div className="wrap">
+                <h2>Not Found</h2>
+                <p>Sorry! We couldn't find the page you're looking for.</p>
+            </div>
+        </main>
+    );
 };
 
 export default NotFound;
